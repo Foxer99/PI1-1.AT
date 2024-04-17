a148cc1f4c6e63b7a577b1f54c644a2f076e8b31
Filip Roman Michel <michelfiliproman@gmail.com>
2024-04-17 10:38:30 +0200

    Pridané spoluhlasky

--- 008-1.ročník(3.4.2024)/4.py
+++ 008-1.ročník(3.4.2024)/4.py
@@ -1,8 +1,14 @@
 veta = input('Napíš vetu:\t')
 pocet_samohlasiek = 0
+pocet_spoluhlasiek = 0
 
 for i in veta:
     if i in 'aAáÁäÄeEéÉiIíÍyYýÝoOóÓuUúÚ':
         pocet_samohlasiek += 1
 
+for i in veta:
+    if i in 'qQwWrŕRŔtTzžZŽpPsšSŠdďDĎfFgGhHjJkKlĺLĹxXcčCČvVbBnňNŇmM':
+        pocet_spoluhlasiek +=1
+
 print(pocet_samohlasiek)
+print(pocet_spoluhlasiek)
